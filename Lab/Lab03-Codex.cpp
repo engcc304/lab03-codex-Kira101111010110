83eeed6e8113604aae8723a33a617a3143a5543f
Kira101111010110 <137255457+Kira101111010110@users.noreply.github.com>
2023-07-20 23:53:10 +0700

    updatelab03

--- Lab/Lab03-Codex.cpp
+++ Lab/Lab03-Codex.cpp
@@ -11,11 +11,11 @@ int main(void) {
     printf( "Enter customer name [QUIT to stop]: " ) ;
     scanf( "%s", CustomerName ) ;
 
-    while ( strcmp(CustomerName, "QUIT") != 0 ) {
+    while ( strcmp( CustomerName, "QUIT" ) != 0 ) {
         i = 0;
         printf( "Enter name, quantity, and price for item %d [END 0 0]: " , i + 1 ) ;
         scanf( "%s %d %f", Name[i], &Quantity[i], &UnitPrice[i] ) ;
-        while ( strcmp(Name[i], "END") != 0 ) {
+        while ( strcmp( Name[i], "END" ) != 0 ) {
             i++;
             printf( "Enter name, quantity, and price for item %d [END 0 0]: " , i + 1 ) ;
             scanf( "%s %d %f", Name[i], &Quantity[i], &UnitPrice[i] ) ;
@@ -23,21 +23,20 @@ int main(void) {
         NumberOfItems = i ;
 
         TotalAmount = 0.0 ;
-        for ( i = 0; i < NumberOfItems; i++ ) {
+        for ( i = 0 ; i < NumberOfItems ; i++ ) {
             TotalPrice[i] = Quantity[i] * UnitPrice[i];
             TotalAmount += TotalPrice[i];
-        }//end for
-
+        } //end for
         printf( "--------------------------------------------------------------\n" ) ;
         printf( "%-20s %11s %13s %17s\n", "Name", "Quantity", "Unit Price", "Total Price" ) ;
         printf( "--------------------------------------------------------------\n" ) ;
-        for ( i = 0; i < NumberOfItems; i++) {
+        for ( i = 0 ; i < NumberOfItems ; i++ ) {
             printf( "%-20s %11d %13.2f %17.2f\n", Name[i], Quantity[i], UnitPrice[i], TotalPrice[i] ) ;
-        }//end for
+        } //end for
         printf( "--------------------------------------------------------------\n" ) ;
         printf( "%52s %11.2f\n", "TOTAL AMOUNT : ", TotalAmount) ;
         printf( "Enter customer name [QUIT to stop]: ") ;
         scanf( "%s", CustomerName ) ;
-    }//end while
+    } //end while
     return 0;
-}//end function
+} //end function
